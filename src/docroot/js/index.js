aa07ffdc0141178b62db59035e30af3d85addea5
noliaki <noliaki@gmail.com>
2017-08-18 14:26:56 +0900

    :+1: add flowtypes

--- src/docroot/js/index.js
+++ src/docroot/js/index.js
@@ -1,1 +1,19 @@
-console.log('template-single-page')
+// @flow
+
+interface Hoge {
+  ho: string,
+  fu: number
+}
+
+const awesome: Hoge = {
+  ho: false,
+  fu: 9
+}
+
+class Fuga {
+  static fiz: number = 10
+}
+
+Fuga.fiz = 20
+
+console.log(awesome.ho)
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,15 @@
 const path = require('path')
 const webpack = require('webpack')
+const FlowStatusWebpackPlugin = require('flow-status-webpack-plugin')
+const plugins = [
+  new FlowStatusWebpackPlugin({
+    failOnError: true
+  }),
+  new webpack.DefinePlugin({
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+  })
+]
+const distDir = process.env.DIST_DIR || './dist'
 
 const config = {
   context: path.resolve('./src/docroot', 'js/'),
@@ -7,43 +17,37 @@ const config = {
     index: './index.js'
   },
   output: {
-    path: path.resolve('./dist', 'js'),
+    path: path.resolve(distDir, 'js'),
     filename: '[name].js'
   },
   module: {
     rules: [
       {
         test: /\.js$/,
-        exclude: /node_modules/,
-        use: [{
-          loader: 'babel-loader',
-          query: {
-            presets: ['env']
-          }
-        }]
+        loader: 'babel-loader',
+        exclude: /node_modules/
       }
     ]
   },
-  plugins: [
+  plugins: plugins.concat([
     new webpack.optimize.UglifyJsPlugin({
       compress: {
         warnings: false,
         drop_console: true
       }
     })
-  ]
+  ])
 }
 
 if (process.env.NODE_ENV === 'development') {
-  config.output.path = path.resolve('./dist-dev', 'js')
   config.watch = true
   config.cache = true
   config.devtool = 'source-map'
-  config.plugins = [
+  config.plugins = plugins.concat([
     new webpack.LoaderOptionsPlugin({
       debug: true
     })
-  ]
+  ])
 }
 
 module.exports = config
