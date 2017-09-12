7ae2d701020389cab838e273135824161d1f3b37
noliaki <noliaki@gmail.com>
2017-09-12 16:50:10 +0900

    :+1: make stylus render script

---
+++ script/stylus.js
@@ -0,0 +1,52 @@
+const stylus = require('stylus')
+const autoprefixer = require('autoprefixer-stylus')
+const fs = require('fs')
+const path = require('path')
+const glob = require('glob')
+const shell = require('shelljs')
+
+const src = require('./config').src
+const dist = require('./config').dist
+const docRoot = require('./config').docroot
+
+const files = glob.sync(`${docRoot}/**/*.styl`)
+
+const convertStylus = function (filename) {
+  const str = fs.readFileSync(filename, {
+    encoding: 'utf8'
+  })
+
+  stylus(str)
+    .include(`${src}/modules/stylus`)
+    .use(autoprefixer({
+      browsers: ['ie >= 6']
+    }))
+    .set('compress', true)
+    .render((err, output) => {
+      if (err) throw err
+
+      writeFile(filename, output)
+    })
+}
+
+files.forEach(file => {
+  convertStylus(file)
+})
+
+function writeFile (filename, string) {
+  const distPath = path.resolve(dist, path.relative(docRoot, filename))
+
+  fs.writeFile(distPath.replace(/\.styl/, '.css'), string, error => {
+    if (error) {
+      if (error.code === 'ENOENT') {
+        shell.mkdir('-p', path.dirname(distPath))
+      } else {
+        throw error
+      }
+    }
+
+    console.log(`WRITTEN: ${distPath}`)
+  })
+}
+
+module.exports = convertStylus
